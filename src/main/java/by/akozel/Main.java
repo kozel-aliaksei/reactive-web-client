077f60c20e1c05459d7a3d8a91c5ed87711921d9
akozel <to.akozel@gmail.com>
2024-07-10 15:17:58 +0400

    initial commit

--- src/main/java/by/akozel/Main.java
+++ src/main/java/by/akozel/Main.java
@@ -2,6 +2,6 @@ package by.akozel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
     }
 }
